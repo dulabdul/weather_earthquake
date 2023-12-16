924baa83a81e5b221247072b56c922e7025454aa
dulabdul <dul.abdoel21@gmail.com>
2023-12-16 11:26:02 +0800

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        greenColor: '#028A80',
+        textPrimary: '#444E72',
+        textSecondary: '#3C3C43',
       },
     },
   },
   plugins: [],
-}
+};
